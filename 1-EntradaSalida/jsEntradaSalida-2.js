3e1e394c0b47fd897916091f9a4c5548a936ffe7
Marini Daniel Julio <40776480+MariniDaniel@users.noreply.github.com>
2018-07-02 21:55:21 -0300

    Primer dia de curso
    Programacion Inicial Curso de ingreso UTN

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,19 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre;
+	//nombre="Daniel";
+	//alert(nombre);
+	nombre=prompt("Ingrese su nombre","Natalia Natalia");
+	alert("Su nombre es: "+nombre);
+
+
+
+
+
+	//var person = prompt("Please enter your name", "Harry Potter");
+	//Esto lo sacamos de W3schools.com
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,14 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre;
+	nombre=document.getElementById('elNombre').value; //Siempre se utiliza getelementbyid
+	alert(nombre);
+
+
+
+	//1 Document  2 .get  3 Tab  4 Shift + i para getid
+
 }
 
 
