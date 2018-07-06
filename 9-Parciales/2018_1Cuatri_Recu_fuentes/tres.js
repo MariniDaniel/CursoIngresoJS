9f5261857f716b8e17d30bac3a53adcedd01a27e
Marini Daniel Julio <40776480+MariniDaniel@users.noreply.github.com>
2018-07-05 21:57:46 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,22 @@
 function mostrar()
 {
 
+
+	var precioInicial;
+	var descuento;
+	var cuenta;
+	var iva;
+	
+	precioInicial=prompt("Ingrese precio : ");
+	descuento=precioInicial*0.50;
+	cuenta=precioInicial-descuento;
+	iva=precioInicial*0.21;
+
+	alert(" el precio con descuento es : "+cuenta+" y el iva es :  "+iva);
+	document.getElementById('elPrecioFinal').value=descuento+iva;
+	
+
+
+
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+  
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
+
+	base=prompt("Ingrese la base");
+	altura=prompt("Ingrese la altura");
+	
+	
+
+
 
 }
