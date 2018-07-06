f0d6d5dbe2ece8b910f9f5eab96ab7cccd674514
Marini Daniel Julio <40776480+MariniDaniel@users.noreply.github.com>
2018-07-05 21:56:42 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,6 +1,18 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad;
+	edad=document.getElementById('edad').value;
+	
+	if (edad==15) {
+
+		alert("Niña bonita");
+
+
+	}
+	else{
+		alert("No tiene 15 años")
+	}
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,25 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+	edad=document.getElementById('edad').value;
+	
+	if (edad>=13 && edad<=17 ) {
+
+		alert("Es adolescente");
 
 
+  }
+
+  if(edad>=18){
+  	alert("Es mayor de edad");
+
+  }
+
+
+  if(edad<13){
+  	alert("Es menor a 13");
+
+  }
 
 }//FIN DE LA FUNCIÓN
