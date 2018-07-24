236c852b0e6784e4f746512f22bcff2158a11ad6
Marini Daniel Julio <40776480+MariniDaniel@users.noreply.github.com>
2018-07-23 22:19:31 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,154 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+ 	var precioUnitario= 35;//variable num
+ 	var marca;
+ 	var cantidadLamparas; 	
+ 	var precioFinal;
+ 	var descuento=1;
+ 		marca=document.getElementById('Marca').value;
+ 		cantidadLamparas=document.getElementById("Cantidad").value;
+ 		cantidadLamparas=parseInt(cantidadLamparas);
+ 		precioBruto=precioUnitario *cantidadLamparas;
+
+
+ 		switch(cantidadLamparas){
+
+ 			case 1:
+ 			case 2:
+ 			
+ 				break;
+ 			case 3:
+ 				switch(marca){
+ 			case "ArgentinaLuz":
+ 				descuento=precioBruto*0,85;
+ 				break;
+ 			case "FelipeLamparas":
+ 				descuento=precioBruto*0,90;
+ 				break;
+
+ 			default:
+ 				descuento=precioBruto*0,95;
+
+ 				}
+ 				
+
+
+
+
+
+ 		}
+
+ 		precioFinal=precioBruto*descuento;
+ 		document.getElementById('precioDescuento').value=precioFinal;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ 		if(cantidadLamparas>=6){
+
+ 			descuento=precioBruto*0,5;
+
+ 		}
+
+ 		else if(cantidadLamparas==5 && marca=="ArgentinaLuz"){
+
+ 			descuento=precioBruto*0,4;
+
+
+ 		}
+
+ 		else if(cantidadLamparas==5){//no hace falta poner ArgentinaLuz porque entra en el if anterior.
+
+ 			descuento=precioBruto*0,3;
+
+ 		}
+
+ 		else if(cantidadLamparas==4){
+ 			if(marca=="ArgentinaLuz" \\ marca=="FelipeLamparas");
+
+ 			descuento=precioBruto*0,25;
+
+ 			else
+ 				descuento=precioBruto*0,2;
+
+ 		}	
+
+
+ 		else if(cantidadLamparas==3 && marca=="FelipeLamparas"){
+
+ 			descuento=precioBruto*0,1;
+
+ 		else if(cantidadLamparas==3 && marca=="ArgentinaLuz");
+
+ 			descuento=precioBruto*0,15;
+
+ 		else if(cantidadLamparas==3){
+
+ 			descuento=precioBruto*0,05;
+
+ 		}
+ 		else
+ 			descuento=0;
+
+ 		}
+
+ 		precioFinal=precioBruto-descuento;
+
+ 		if(precioFinal>120){
+
+ 			ingresosBrutos=precioFinal*0,1;
+ 			precioFinal=precioFinal+ingresosBrutos;
+ 			mensaje="El precio es"+precioFinal+"es"+ingresosBrutos;
+
+		}
+		 else{
+		 	mensaje="El precio es"+precioFinal;
+		 }
+
+
+		 alert(mensaje);
+
+
+*/
+
 }
--- 8-TPs/jsPiedarPapelTijera2.js
+++ 8-TPs/jsPiedarPapelTijera2.js
@@ -2,30 +2,99 @@ var eleccionMaquina;
 var ContadorDeEmpates=0;
 var ContadorDeGanadas=0;
 var ContadorDePerdidas=0;
+var piedra=1;
+var papel=2;
+var tijera=3;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 3
-	 	
+	eleccionMaquina=math.floor(math.random()*3)+1;	
 
 
 
 }//FIN DE LA FUNCIÓN
+
+
+
 function piedra()
 {
-	
+	if(eleccionMaquina==1){
+		ContadorDeEmpates++;//contador++   ++contador  contador+=1; o  contador=contador+1;
+		alert("Empato");
+	}
+	else if(eleccionMaquina==2){
+
+		ContadorDeGanadas++;
+		alert("Gano");
+
+	}
+	else{
+
+		ContadorDePerdidas++;
+		alert("Perdio")
+	}
+
+
+	mostrarResultado()
+	comenzar()	
 
 }//FIN DE LA FUNCIÓN
+
 function papel()
 {
 
+if(eleccionMaquina==2){
+		ContadorDeEmpates++;//contador++   ++contador  contador+=1; o  contador=contador+1;
+		alert("Empato");
+	}
+	else if(eleccionMaquina==1){
+
+		ContadorDeGanadas++;
+		alert("Gano");
+
+	}
+	else{
+
+		ContadorDePerdidas++;
+		alert("Perdio")
+	}
+
+	mostrarResultado()
+	comenzar()
 }//FIN DE LA FUNCIÓN
+
+
 function tijera()
 {
+
+if(eleccionMaquina==3){
+		ContadorDeEmpates++;//contador++   ++contador  contador+=1; o  contador=contador+1;
+		alert("Empato");
+	}
+	else if(eleccionMaquina==1){
+
+		ContadorDeGanadas++;
+		alert("Gano");
+
+	}
+	else{
+
+		ContadorDePerdidas++;
+		alert("Perdio")
+	}
 	
+	mostrarResultado()
+	comenzar()
 }//FIN DE LA FUNCIÓN
 
 function mostrarResultado()
 {
+
+document.getElementById('Ganadas: ').value=ContadorDeGanadas;
+document.getElementById('Empatadas: ').value=ContadorDePerdidas;
+document.getElementById('Perdidas: ').value=ContadorDePerdidas;
+
+
 
 }
